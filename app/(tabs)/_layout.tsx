7888984a0fef97344fafebed5522f471602669f7
FelixDeck <mzpro123@gmail.com>
2026-05-28 21:46:04 -0500

    perfil

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -30,6 +30,15 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      
+      <Tabs.Screen
+        name="perfil"
+        options={{
+          title: 'Profile',
+          // Usamos provisionalmente "house.fill" para descartar que sea un error del ícono
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />, 
+        }}
+/>
     </Tabs>
   );
 }
